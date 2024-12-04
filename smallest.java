17639e6e497f57f04076c098d99d0ba5fe720bd2
mahamood123-o <shaikmisba305@gmail.com>
2024-12-04 14:20:42 +0530

    Add files via upload

---
+++ smallest.java
@@ -0,0 +1,20 @@
+class smallest
+{
+    public static void main(String args[]){
+        int a=10;
+        int b=2;
+        int c=8;
+        if(a<b&&a<c)
+        {
+            System.out.println("a is smaller");
+        }
+        else if(b<a&&b<c)
+        {
+            System.out.println("b is smaller");
+        }
+        else
+        {
+            System.out.println("c is smaller");
+        }
+    }
+}
