030a059fd945b2388ca5e3409aab541882b82381
mahamood123-o <shaikmisba305@gmail.com>
2024-12-04 12:23:42 +0530

    Add files via upload

---
+++ flip.java
@@ -0,0 +1,17 @@
+public class flip
+{
+    public static void main(String args[]){
+        int a=10;
+        int pos=2;
+        if((a&pos)==1)
+        {
+            System.out.println("setbit");
+        }
+        else
+        {
+                System.out.println("not a setbit");
+
+        }
+        
+    }
+}
