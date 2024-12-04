77bf52de8048190d19643da8c7be39623366f9ab
mahamood123-o <shaikmisba305@gmail.com>
2024-12-04 11:01:17 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class a
+{
+public static void main(String args[])
+{
+ int a=4;
+ if((a&1)==0)
+    {
+        System.out.println("even");
+    }
+    else
+    {
+        System.out.println("odd");
+    }
+}
+}
