384557ebda48fb55d949245f0526d1cbde9d5f30
mahamood123-o <shaikmisba305@gmail.com>
2024-12-04 12:40:50 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,17 @@
+public class bitwise
+{
+    public static void main(String args[]){
+        int n=16;
+        if((n&(n-2))==0)
+        {
+            System.out.println("multiple of 4");
+        }
+        else
+        {
+            System.out.println("not multiple of 4");
+
+        }
+        
+    }
+    
+}
