f67d055b4200ce70bafcb6c6af39b865643541f4
mahamood123-o <shaikmisba305@gmail.com>
2024-12-04 10:11:24 +0530

    Add files via upload

---
+++ dss.java
@@ -0,0 +1,9 @@
+class rightshift
+{
+    public static void main(String args[])
+    {
+        System.out.println(16>>3);
+        System.out.println(22>>2);
+        System.out.println(28>>3);
+    }
+}
